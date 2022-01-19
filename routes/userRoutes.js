92cc18d532fa417d5ca6cfcd27f1793dd8939a79
Carlos N Reina <cnreina@gmail.com>
2022-01-18 20:27:02 -0700

    user views

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,13 @@ const APP_CWD = process.cwd();
 const userController = require(APP_CWD + '/controllers/userController');
 const router = express.Router();
 
-router.get('/user', userController.userView);
-router.get('/user/cart', userController.userCartView);
-router.get('/user/cart/edit-item/:itemuuid', userController.editCartItemView);
+// router.get('/user', userController.userView);
+// router.get('/user/cart', userController.userCartView);
+// router.get('/user/cart/edit-item/:itemuuid', userController.editCartItemView);
 
-router.post('/user/add-item', userController.addCartItemByUUID);
-router.post('/user/cart/update-item', userController.updateCartItemByUUID);
-router.post('/user/cart/remove-item', userController.removeCartItemByUUID);
+// router.post('/user/add-item', userController.addCartItemByUUID);
+// router.post('/user/cart/update-item', userController.updateCartItemByUUID);
+// router.post('/user/cart/remove-item', userController.removeCartItemByUUID);
 
 module.exports = router;
+
