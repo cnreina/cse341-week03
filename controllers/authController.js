d5ca77633621f8c5162e5f5f99bf21293ab814a2
Carlos N Reina <cnreina@gmail.com>
2022-01-20 14:06:27 -0700

    log-in and log-out

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,3 +29,13 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getSignup = (req, res, next) => {
+  res.render('authViews/signupView', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
+exports.postSignup = (req, res, next) => {};
